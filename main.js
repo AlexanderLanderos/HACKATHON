d755ab5cc47416d33c53bb7c5a5cc297c0d1bd25
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-08-20 11:36:51 -0700

    check

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-let artist = '#artist'
-let song = '#song'
+let artist = document.querySelector('#artist').value;
+let song = document.querySelector('#song').value;
 //let url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
-let url;
+let url = `https://api.lyrics.ovh/v1/thrice/deadbolt`;
+
+
 
 $(document).on('ready', () => {
-  console.log(artist, song)
-
+  console.log("yess")
   fetch(url)
   .then(res => res.json())
   .then(getData => {
-    console.log(getData)
+    document.querySelector('#test').innerHTML = getData.lyrics;
   })
-}
-
-
-
+  .catch(err => console.log(err));
+});
